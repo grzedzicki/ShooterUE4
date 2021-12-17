e642b8744c19b64a65a276899a40359f8393c22a
grzedzicki <49614558+grzedzicki@users.noreply.github.com>
2021-12-17 10:58:21 +0100

    Create README.m

---
+++ MainCharacter/README.m
@@ -0,0 +1,9 @@
+[Powrót](../README.md)<br />
+ 
+[Unreal/C++](https://github.com/grzedzicki/ShooterUE4/tree/main/MainCharacter)
+  
+Blueprint:  
+CTRL+SCROLL oddalenie/przybliżenie  
+Poruszanie się prawym przyciskiem myszy  
+<iframe width=1280 height=650 src="https://blueprintue.com/render/q6u-pstt/" scrolling="no" allowfullscreen></iframe>
+
